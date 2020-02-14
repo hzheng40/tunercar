119051952bb420c8a03a02093abd7c6b1760ddf5
billyzheng <billyzheng.bz@gmail.com>
2020-02-14 15:20:14 -0500

    added example processed results, updated plotting script with seaborn

--- benchmark/plotting/plot_result.py
+++ benchmark/plotting/plot_result.py
@@ -2,23 +2,48 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-import sys
-sys.path.append('../results/')
+import os
+# import sys
+# sys.path.append('../results/')
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--result_npz', type=str, required=True)
+parser.add_argument('--results_path', type=str, required=True)
+parser.add_argument('--save_csv', type=int, default=1)
+parser.add_argument('--csv_path', type=str, default='../results/processed_population_time.csv')
+parser.add_argument('--reprocess', type=int, default=1)
 args = parser.parse_args()
 
-hi = np.load(args.result_npz)
-hi = hi['time_list_hist']
-df = pd.DataFrame(columns=['gen', 'score'])
+if args.reprocess:
+    npz_list = []
+    all_time_hist_list = []
+    exp_name_list = []
+    for filename in os.listdir(args.results_path):
+        if filename.endswith('.npz'):
+            exp_name = os.path.splitext(filename)[0]
+            hi = np.load(args.results_path+filename)
+            npz_list.append(hi)
+            all_time_hist_list.append(hi['time_list_hist'])
+            exp_name_list.append(exp_name)
+        else:
+            continue
 
-for i in range(hi.shape[0]):
-    for j in range(hi.shape[1]):
-        if hi[i, j] < 100 and hi[i, j] > 0:
-            df = df.append({'gen': i, 'score':hi[i, j]}, ignore_index=True)
+    df = pd.DataFrame(columns=['exp_name', 'gen', 'score'])
 
-sns.set_style('dark')
-ax = sns.lineplot(data=df, x='gen', y='score', ci='sd', markers=True)
+    for k in range(len(npz_list)):
+        exp_name = exp_name_list[k]
+        print('Processing run', exp_name)
+        time_hist = all_time_hist_list[k]
+        for i in range(time_hist.shape[0]):
+            for j in range(time_hist.shape[1]):
+                if time_hist[i, j] < 100 and time_hist[i, j] > 0:
+                    df = df.append({'exp_name': exp_name, 'gen': i, 'score':time_hist[i, j]}, ignore_index=True)
+    if args.save_csv:
+        df.to_csv(args.csv_path)
+
+else:
+    df = pd.read_csv(args.csv_path, index_col=0)
+
+sns.set_style('white')
+ax = sns.lineplot(data=df, x='gen', y='score', hue='exp_name', ci='sd', palette='pastel')
 plt.show()
