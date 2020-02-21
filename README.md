90d751c278f741d4830582489e111a0452b45ec0
billyzheng <billyzheng.bz@gmail.com>
2020-02-21 03:02:25 -0500

    Processed result data, updating utils

--- README.md
+++ README.md
@@ -12,3 +12,15 @@ If you find this code useful in your work, please consider citing:
   title={TUNERCAR: A Superoptimization Toolchain for Autonomous Racing},
   author={O’Kelly, Matthew and Zheng, Hongrui and Auckley, Joseph and Jain, Achin and Luong, Kim and Mangharam, Rahul}
 }
+```
+
+This implementation also uses the F1/10 simulator from: [https://github.com/hzheng40/f110_gym]()
+
+If you find the simulator useful in your work, please consider citing:
+
+```
+@article{TODO,
+  title={TODO},
+  author={TODO}
+}
+```
--- python/utils.py
+++ python/utils.py
@@ -1,5 +1,5 @@
 import numpy as np
-from scipy.interpolate import splprep, splev
+from scipy.interpolate import splprep, splev, spalde
 import csv
 import yaml
 from PIL import Image
@@ -30,20 +30,32 @@ def make_spline(points, config):
     new_u = np.arange(spline_size)
     new_u = new_u / float(spline_size - 1)
 
-    # evaluate spline
-    spline = np.asarray(splev(new_u, tck))
+    # evaluate spline, out is Nx2
+    spline = np.asarray(splev(new_u, tck)).T
 
-    return spline
+    # get thetas
+    derivs = spalde(new_u, tck)
+    Dx = np.asarray(derivs[0])
+    Dy = np.asarray(derivs[1])
+    dx = Dx[:, 1]
+    dy = Dy[:, 1]
+    ddx = Dx[:, 2]
+    ddy = Dy[:, 2]
+    curvature = (dx*ddy - dy*ddx)/((dx*dx + dy*dy)**1.5)
+    theta = np.arctan2(dy, dx)
 
-@njit(fastmath=False, cache=True)
-def get_bbox(states):
-    # states (Nx3), returns (4xNx2)
-    poses = np.expand_dims(states[:, :2], axis=0)
-    Lengths = np.expand_dims(CAR_LENGTH*np.ones((states.shape[0])), axis=1)
-    Widths = np.expand_dims(CAR_WIDTH*np.ones((states.shape[0])), axis=1)
-    x = np.expand_dims((Lengths/2.)*np.vstack((np.cos(states[:,2]), np.sin(states[:,2]))).T, axis=0)
-    y = np.expand_dims((Widths/2.)*np.vstack((-np.sin(states[:,2]), np.cos(states[:,2]))).T, axis=0)
+    return spline, theta, curvature
+
+# @njit(cache=True)
+def get_bbox(waypoints, box_width, box_height):
+    # waypoints (Nx3) [x, y, theta], returns (4xNx2)
+    poses = np.expand_dims(waypoints[:, :2], axis=0)
+    Lengths = np.expand_dims(box_height*np.ones((waypoints.shape[0])), axis=1)
+    Widths = np.expand_dims(box_width*np.ones((waypoints.shape[0])), axis=1)
+    x = np.expand_dims((Lengths/2.)*np.vstack((np.cos(waypoints[:,2]), np.sin(waypoints[:,2]))).T, axis=0)
+    y = np.expand_dims((Widths/2.)*np.vstack((-np.sin(waypoints[:,2]), np.cos(waypoints[:,2]))).T, axis=0)
     corners = np.concatenate((x-y, x+y, y-x, -x-y), axis=0)
+    # 4xNx4, middle dim is num boxes
     return corners + poses
 
 def get_width(center_line, config):
