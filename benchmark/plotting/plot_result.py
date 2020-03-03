83f991aa102f5768274d709d84491129a5fb82bf
billyzheng <billyzheng.bz@gmail.com>
2020-03-02 21:03:07 -0500

    updated plotting, need to add subplot inset

--- benchmark/plotting/plot_result.py
+++ benchmark/plotting/plot_result.py
@@ -12,8 +12,25 @@ parser.add_argument('--results_path', type=str, required=True)
 parser.add_argument('--save_csv', type=int, default=1)
 parser.add_argument('--csv_path', type=str, default='../results/processed_population_time.csv')
 parser.add_argument('--cov_norm_csv_path', type=str, default='../results/processed_population_covnorm.csv')
-parser.add_argument('--reprocess', type=int, default=1)
+parser.add_argument('--reprocess', type=int, default=0)
+parser.add_argument('--clean_score_csv', type=int, default=0)
+parser.add_argument('--clean_score_csv_path', type=str, default='../results/processed_population_time_cleaned.csv')
 args = parser.parse_args()
+
+if args.clean_score_csv:
+    df = pd.read_csv(args.csv_path, index_col=0)
+    cleaned_df = pd.DataFrame(columns=['population_size', 'percentile', 'generation', 'score'])
+    for index, row in df.iterrows():
+        exp_name = row['experiment']
+        exp_split = exp_name.split('e')
+        pop_size = int(exp_split[1][:-1])
+        percentile = float(exp_split[-1])
+        new_row = {'population_size': pop_size, 'percentile': percentile, 'generation': row['generation'], 'score': row['score']}
+        cleaned_df = cleaned_df.append(new_row, ignore_index=True)
+    cleaned_df.to_csv(args.clean_score_csv_path)
+
+else:
+    cleaned_df = pd.read_csv(args.clean_score_csv_path, index_col=0)
 
 if args.reprocess:
     npz_list = []
@@ -63,13 +80,39 @@ else:
     cov_norm_df = pd.read_csv(args.cov_norm_csv_path, index_col=0)
 
 sns.set_style('white')
-ax = sns.lineplot(data=df, x='gen', y='score', hue='exp_name', ci='sd', palette='pastel')
+sns.set_style('ticks')
+sns.set_context('poster')
+ax = sns.lineplot(data=df, x='generation', y='score', hue='experiment', ci='sd', palette='Paired')
+ax.set_xscale('log')
 plt.show()
 
+
 sns.set_style('white')
-ax1 = sns.lineplot(data=cov_norm_df, x='gen', y='cov_norm', hue='exp_name', palette='pastel')
+sns.set_style('ticks')
+sns.set_context('poster')
+palette = sns.color_palette("mako_r", 5)
+ax = sns.lineplot(data=cleaned_df[cleaned_df['population_size']==1000], x='generation', y='score', hue='percentile', ci='sd', palette=palette)
+ax.set_xscale('log')
+# ax.set_yscale('log')
+plt.show()
+
+
+sns.set_style('white')
+sns.set_style('ticks')
+sns.set_context('poster')
+palette = sns.color_palette("mako_r", 6)
+ax = sns.lineplot(data=cleaned_df[cleaned_df['percentile']==0.01], x='generation', y='score', hue='population_size', ci='sd', palette=palette)
+ax.set_xscale('log')
+# ax.set_yscale('log')
+plt.show()
+
+
+sns.set_style('white')
+sns.set_style('ticks')
+sns.set_context('poster')
+ax1 = sns.lineplot(data=cov_norm_df, x='generation', y='covariance_norm', hue='experiment', palette='Paired')
 ax1.axhline(0.01, ls='--')
-ax1.text(2,0.012, "Termination Threshold")
-ax1.set_xscale('log')
+ax1.text(115,0.007, "Termination Threshold")
+# ax1.set_xscale('log')
 ax1.set_yscale('log')
 plt.show()
