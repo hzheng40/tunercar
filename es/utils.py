a596c92902477a3a6ec23381d58090bf1c7735d2
billyzheng <billyzheng.bz@gmail.com>
2021-03-03 00:50:41 -0500

    added linear velocity interp

--- es/utils.py
+++ es/utils.py
@@ -37,8 +37,61 @@ def perturb(vec, waypoints, track_width, smoothing=20):
 
     return out_smooth
 
+def get_curvature(waypoints):
+    """
+    Returns the curvature of each point on the list of waypoints
+
+    Args:
+        waypoints (numpy.ndarray (n, 2)): list of waypoints
+
+    Returns:
+        curvature (numpy.ndarray (n, )): curvature at each point of the waypoint
+    """
+    
+    # no smoothing, since the given waypoints should already be smoothed
+    tck, u = scipy.interpolate.splprep([waypoints[:, 0], waypoints[:, 1]], s=0, k=5, per=True)
+    unew = np.arange(0, 1.0, 0.001)
+    derivs = scipy.interpolate.spalde(unew, tck)
+
+    # getting the curvature
+    Dx = np.asarray(derivs[0])
+    Dy = np.asarray(derivs[1])
+    dx = Dx[:, 1]
+    dy = Dy[:, 1]
+    ddx = Dx[:, 2]
+    ddy = Dy[:, 2]
+    curvature = (dx*ddy - dy*ddx)/((dx*dx + dy*dy)**1.5)
+
+    return curvature
+
+def interpolate_velocity(min_vel, max_vel, waypoints, method='linear', optional_args=[]):
+    """
+    Interpolate to find velocity along the waypoints based on curvature
+
+    Args:
+        min_vel (float): Minimum velocity at maximum curvature
+        max_vel (float): Maximum velocity at minimum curvature
+        waypoints (numpy.ndarray(n, 2)): Waypoints to find velocity on, columns are (x, y), after perturb
+        method (str, default='linear'): method of interpolation, default is linear, options include ['linear', 'sigmoid', ]
+        optional_args (list(str), default=[]): optional arguments for different interpolation methods
+
+    Returns:
+        vel (numpy.ndarray(n, )):
+    """
+    # interp data
+    curvature = np.abs(get_curvature(waypoints))
+    x = np.array([np.min(curvature), np.max(curvature)])
+    y = np.array([max_vel, min_vel])
+    
+    if method == 'linear':
+        f = scipy.interpolate.interp1d(x, y, kind='linear')
+        vel = f(curvature)
+
+    return vel
+
 # unit tests
 import unittest
+import matplotlib.pyplot as plt
 
 class PerturbTest(unittest.TestCase):
     def setUp(self):
@@ -49,6 +102,10 @@ class PerturbTest(unittest.TestCase):
         raw_waypoints = np.loadtxt('maps/f1tenth_racetracks/Austin/Austin_map_waypoints.csv', delimiter=',', skiprows=1)
         self.waypoints = raw_waypoints[:, 0:2]
         self.track_width = raw_waypoints[0, 2] = raw_waypoints[0, 3]
+
+        # min max vel
+        self.min_vel = 2.0
+        self.max_vel = 15.0
 
     def test_perturb(self):
         # random perturbation
@@ -62,13 +119,28 @@ class PerturbTest(unittest.TestCase):
         # inner
         new_waypoints2 = perturb(vec2, self.waypoints, self.track_width, smoothing=5)
 
-        import matplotlib.pyplot as plt
         plt.scatter(self.waypoints[:, 0], self.waypoints[:, 1], c='red')
         plt.scatter(new_waypoints1[:, 0], new_waypoints1[:, 1], c='blue')
         plt.scatter(new_waypoints2[:, 0], new_waypoints2[:, 1])
         plt.scatter(new_waypoints[:, 0], new_waypoints[:, 1])
         plt.axes().set_aspect('equal')
         plt.show()
+
+    def test_curvature(self):
+        vec = np.zeros(self.waypoints.shape[0])
+        new_waypoints = perturb(vec, self.waypoints, self.track_width)
+        curvature = get_curvature(new_waypoints)
+        plt.scatter(new_waypoints[:, 0], new_waypoints[:, 1], c=curvature)
+        plt.show()
+        self.assertEqual(1000, curvature.shape[0])
+
+    def test_vel_interp(self):
+        vec = np.zeros(self.waypoints.shape[0])
+        new_waypoints = perturb(vec, self.waypoints, self.track_width)
+        vel = interpolate_velocity(self.min_vel, self.max_vel, new_waypoints)
+        plt.scatter(new_waypoints[:, 0], new_waypoints[:, 1], c=vel)
+        plt.show()
+        self.assertEqual(1000, vel.shape[0])
 
 if __name__ == '__main__':
     unittest.main()
