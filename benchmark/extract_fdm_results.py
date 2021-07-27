ae3a0b59b021a5f0cbe91c78aac556b44fdba7dc
billyzheng-mlab <billyzheng.bz@gmail.com>
2021-07-27 19:41:12 -0400

    updated extraction and plotting script

--- benchmark/extract_fdm_results.py
+++ benchmark/extract_fdm_results.py
@@ -14,6 +14,7 @@ args = parser.parse_args()
 
 # list of dfs for all data
 dfs = []
+pf_dfs = []
 
 for exp in args.exp_name:
     print('Extracting: ' + exp)
@@ -22,18 +23,20 @@ for exp in args.exp_name:
     optim = ng.optimizers.base.Optimizer.load(args.pkl_dir + exp + '_optim.pkl')
 
     # pareto front
+    pfdf = pd.DataFrame(columns=['exp_name', 'arm_length', 'num_batt', 'batt_v', 'batt_cap', 'batt_m', 'Max Distance', 'Max Hover Time'])
     print('Pareto Front')
     for param in sorted(optim.pareto_front(), key=lambda p:p.losses[0]):
-        print(f'{param.args[0]} with losses {param.losses}')
+        # print(f'{param.args[0]} with losses {param.losses}')
+        ind = param.args[0]
+        pfdf = pfdf.append({'exp_name': exp, 'arm_length': ind['arm_length'], 'num_batt': ind['num_batt'], 'batt_v': ind['batt_v'], 'batt_cap': ind['batt_cap'], 'batt_m': ind['batt_m'], 'Max Distance': -param.losses[0], 'Max Hover Time': -param.losses[1]}, ignore_index=True)
+    pf_dfs.append(pfdf)
 
     batch_size = optim.num_workers
-    # batch_size = 24
     if optim.name == 'CMA':
         batch_size = optim._popsize
     elif optim.name == 'NoisyDE' or optim.name == 'TwoPointsDE' or optim.name[:21] == 'DifferentialEvolution' or optim.name[:13] == 'ConfiguredPSO' or optim.name == 'PSO':
         batch_size = len(optim.population)
     scores_all = data['scores']
-    # params_all = data['params']
     iterations = int(scores_all.shape[0]/batch_size)
 
     # set up dataframes
@@ -41,15 +44,11 @@ for exp in args.exp_name:
 
     # load data into dataframes
     for i in range(iterations):
-        # current_gen_times = []
         for j in range(batch_size):
             max_dist = scores_all[i*batch_size+j, 0]
             max_hov = scores_all[i*batch_size+j, 1]
-            # current_param = params_all[:, i*batch_size+j]
             if max_dist != 0.0 and max_hov != 0.0:
                 df = df.append({'exp_name': exp, 'Iterations': i, 'Max Distance': max_dist, 'Max Hover Time': max_hov}, ignore_index=True)
-        # if len(current_gen_times) > 0:
-        #     cov_norm_df.append({'exp_name': args.exp_name, 'Iterations': i, 'cov_norm': current_time})
     dfs.append(df)
 
 # plot laptimes
@@ -58,18 +57,19 @@ sns.set_style('ticks')
 sns.set_context('poster')
 palette = sns.color_palette("mako_r", len(args.exp_name))
 
-print('Best laptime (2 laps) from each experiment:')
+# plot progress
+for df in dfs:
+    ax = sns.lineplot(data=df, x='Iterations', y='Max Distance', ci='sd', palette='Paired', label=df['exp_name'][0][13:-11])
+plt.show()
 
 for df in dfs:
-    # min_time = df['Score (Lower is better)'].min()
-    # min_idx = df['Score (Lower is better)'].idxmin()
-    # print(df['exp_name'][0] + ': ' + str(min_time) + ' at index: ' + str(min_idx))
-    ax = sns.lineplot(data=df, x='Iterations', y='Max Distance', ci='sd', palette='Paired', label=df['exp_name'][0][29:-11]+'_max_dist')
-    ax = sns.lineplot(data=df, x='Iterations', y='Max Hover Time', ci='sd', palette='Paired', label=df['exp_name'][0][29:-11]+'_max_hover')
-# ax = sns.lineplot(data=df, x='Iterations', y='Score (Lower is better)', ci='sd', palette='Paired')
-# ax.set_xscale('log')
-# plt.xscale('log')
+    ax = sns.lineplot(data=df, x='Iterations', y='Max Hover Time', ci='sd', palette='Paired', label=df['exp_name'][0][13:-11])
 plt.show()
 
 
-# plot spline coverage
+# plot pareto fronts
+for pfdf in pf_dfs:
+    ax = sns.scatterplot(data=pfdf, y='Max Distance', x='Max Hover Time', label=pfdf['exp_name'][0][13:-11])
+    ax = sns.lineplot(data=pfdf, y='Max Distance', x='Max Hover Time')
+ax.set_aspect('equal')
+plt.show()
--- run_docker.sh
+++ run_docker.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-sudo docker run -it --rm --shm-size=10.24gb -v $HOME/tunercar_siemens/es/quad_fdm_runs:/home/ray/tunercar/es/quad_fdm_runs tunercar:fdm
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+sudo docker run -it --rm --shm-size=10.24gb -v $SCRIPT_DIR/es/quad_fdm_runs:/home/ray/tunercar/es/quad_fdm_runs tunercar:fdm
