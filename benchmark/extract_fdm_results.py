d319979eecea445613c9b4ea580de99d862821e8
billyzheng-mlab <billyzheng.bz@gmail.com>
2021-07-27 19:53:02 -0400

    update plotting script

--- benchmark/extract_fdm_results.py
+++ benchmark/extract_fdm_results.py
@@ -24,9 +24,9 @@ for exp in args.exp_name:
 
     # pareto front
     pfdf = pd.DataFrame(columns=['exp_name', 'arm_length', 'num_batt', 'batt_v', 'batt_cap', 'batt_m', 'Max Distance', 'Max Hover Time'])
-    print('Pareto Front')
+    print('Pareto Front for: ' + exp)
     for param in sorted(optim.pareto_front(), key=lambda p:p.losses[0]):
-        # print(f'{param.args[0]} with losses {param.losses}')
+        print(f'{param.args[0]} with max distance: {-param.losses[0]} and max hover time {-param.losses[1]}')
         ind = param.args[0]
         pfdf = pfdf.append({'exp_name': exp, 'arm_length': ind['arm_length'], 'num_batt': ind['num_batt'], 'batt_v': ind['batt_v'], 'batt_cap': ind['batt_cap'], 'batt_m': ind['batt_m'], 'Max Distance': -param.losses[0], 'Max Hover Time': -param.losses[1]}, ignore_index=True)
     pf_dfs.append(pfdf)
@@ -71,5 +71,5 @@ plt.show()
 for pfdf in pf_dfs:
     ax = sns.scatterplot(data=pfdf, y='Max Distance', x='Max Hover Time', label=pfdf['exp_name'][0][13:-11])
     ax = sns.lineplot(data=pfdf, y='Max Distance', x='Max Hover Time')
-ax.set_aspect('equal')
+# ax.set_aspect('equal')
 plt.show()
