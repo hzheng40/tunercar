026e3c9528c5f3b9828fc0fb33b23a8f6796b620
billyzheng-mlab <billyzheng.bz@gmail.com>
2021-08-17 15:44:53 -0400

    update build script

--- build_docker.sh
+++ build_docker.sh
@@ -1,15 +1,24 @@
 #!/usr/bin/env bash
 
-git clone git@git.isis.vanderbilt.edu:SwRI/flight-dynamics-model.git
-#git clone git@github.com:DARPA-SystemicGenerativeEngineering/swri-uav-pipeline.git
-#cd swri-uav-pipeline
-#git submodule init && git submodule update
+[ ! -d "flight-dynamics-model" ] \
+	&& git clone git@git.isis.vanderbilt.edu:SwRI/flight-dynamics-model.git
+[ -d "flight-dynamics-model" ] \
+	&& cd flight-dynamics-model \
+	&& git pull \
+	&& cd ..
 
-mkdir swri-uav-pipeline
-cd swri-uav-pipeline
-git clone git@github.com:DARPA-SystemicGenerativeEngineering/horizon-design-generator.git design-generator
-git clone git@github.com:DARPA-SystemicGenerativeEngineering/uav-cad-models.git
-git clone git@github.com:DARPA-SystemicGenerativeEngineering/uav-design-simulator.git
-git clone git@github.com:DARPA-SystemicGenerativeEngineering/swri-uav-exploration.git
+[ ! -d "swri-uav-pipeline" ] \
+	&& mkdir swri-uav-pipeline \
+	&& cd swri-uav-pipeline \
+	&& git clone git@github.com:DARPA-SystemicGenerativeEngineering/horizon-design-generator.git design-generator \
+	&& git clone git@github.com:DARPA-SystemicGenerativeEngineering/uav-cad-models.git \
+	&& git clone git@github.com:DARPA-SystemicGenerativeEngineering/uav-design-simulator.git \
+	&& git clone git@github.com:DARPA-SystemicGenerativeEngineering/swri-uav-exploration.git \
+	&& cd ..
+
+[ -d "swri-uav-pipeline" ] \
+	&& cd swri-uav-pipeline \
+	&& for d in ./*/ ; do (cd "$d" && git pull); done
+
 cd ..
 sudo docker build -t tunercar:fdm -f Dockerfile .
