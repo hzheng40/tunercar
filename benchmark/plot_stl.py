e8f0d84b3989ee0877f3b58088018d0722488c7f
billyzheng <billyzheng.bz@gmail.com>
2021-10-11 22:55:58 -0400

    updateg

--- benchmark/plot_stl.py
+++ benchmark/plot_stl.py
@@ -4,7 +4,7 @@ from mpl_toolkits import mplot3d
 from matplotlib import pyplot
 from matplotlib.colors import LightSource
 
-def plotSTL(filename):
+def plotShadedSTL(filename):
     # Create a new plot
     figure = pyplot.figure()
     axes = mplot3d.Axes3D(figure)
@@ -14,7 +14,7 @@ def plotSTL(filename):
     polymesh = mplot3d.art3d.Poly3DCollection(stlmesh.vectors)
 
     # Create light source
-    ls = LightSource(azdeg=225, altdeg=45)
+    ls = LightSource(azdeg=225, altdeg=0)
 
     # Darkest shadowed surface, in rgba
     dk = numpy.array([0.2, 0.0, 0.0, 1])
@@ -36,6 +36,19 @@ def plotSTL(filename):
     ctrs = [(min(pts[:,i]) + max(pts[:,i]))/2 for i in range(3)]
     lims = [[ctrs[i] - ptp, ctrs[i] + ptp] for i in range(3)]
     axes.auto_scale_xyz(*lims)
+
+    pyplot.show()
+
+def plotSTL(filename):
+    figure = pyplot.figure()
+    axes = mplot3d.Axes3D(figure)
+
+    stlmesh = mesh.Mesh.from_file(filename)
+    polymesh = mplot3d.art3d.Poly3DCollection(stlmesh.vectors, edgecolors='k', facecolors='w', linewidths=1, alpha=0.5)
+
+    axes.add_collection3d(polymesh)
+    scale = stlmesh.points.flatten()
+    axes.auto_scale_xyz(scale, scale, scale)
 
     pyplot.show()
 
